32aee3bc58d19ad0a902aa9a5798462bb5a960ca
Michael Briquet <michael.br@free.fr>
2022-08-30 21:58:02 +0200

    📦 Add ESLint and Airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: ['airbnb-base'],
+  // add your custom rules here
+  rules: {},
+  settings: {
+    'import/resolver': {
+      alias: {
+        extensions: ['.js', '.json'],
+        map: [
+          ['~', path.resolve(__dirname, './')],
+          ['@', path.resolve(__dirname, './')],
+        ],
+      },
+    },
+  },
+};
